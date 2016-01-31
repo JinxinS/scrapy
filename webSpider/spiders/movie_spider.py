202b8c98f39bf15710ea8ece2aec4f212e6f94f1
SJinxin <jinxin@kth.se>
2016-01-31 01:55:33 +0000

    get dl_link and movie info from each page

--- webSpider/spiders/movie_spider.py
+++ webSpider/spiders/movie_spider.py
@@ -9,22 +9,25 @@ class MSpider(scrapy.Spider):
     base_url = "http://www.ygdy8.net/html/gndy/dyzz/"
 
     def parse(self, response):
-	titles = response.xpath('//table[@class="tbspan"]/tr[2]/td[2]/b/a/text()').extract()
+#	titles = response.xpath('//table[@class="tbspan"]/tr[2]/td[2]/b/a/text()').extract()
         pages = len( response.xpath('//*[@name="sldd"]//option/text()').extract())
         urls  = response.xpath('//*[@name="sldd"]//option/@value').extract()
-        print "number of pages:", pages
-#       for t in titles:
+#        for t in titles:
 #	    print t.encode('utf-8')
-#       for u in urls:
-#          print "".join([MSpider.base_url,u])
 
-        for i in range(1,3):
+        for i in range(0,1):
             url = "".join([MSpider.base_url,urls[i]])
-            print "parsing:",url
-            yield scrapy.Request(url,callback=self.parsePage)
+#            print "parsing page:",i,":",url,"priority=",pages-i
+            yield scrapy.Request(url,callback=self.parsePage,priority=pages-i)
 
-    def parsePage(sel,response):
+    def parsePage(self,response):
         titles = response.xpath('//table[@class="tbspan"]/tr[2]/td[2]/b/a/text()').extract()
-        for t in titles:
-            print t.encode('utf-8')
-        return 
+	urls   = response.xpath('//table[@class="tbspan"]/tr[2]/td[2]/b/a/@href').extract()
+	for t,u in zip(titles,urls):
+            yield scrapy.Request("".join(["http://www.ygdy8.net",u]),callback=self.parseMovie)		
+	return 
+
+    def parseMovie(self,response):
+	meta    = response.xpath('//p[br]/text()').extract()[0]
+	dl_link = response.xpath("//table/tbody/tr/td/a/@href").extract()[0]
+	return	
